b7cdd3609ff549caca9c3d1407e904e0e1fe0806
daveic <88085267+daveic@users.noreply.github.com>
2022-01-27 17:20:38 +0100

    float

--- Models/Transactions.cs
+++ Models/Transactions.cs
@@ -9,7 +9,7 @@ namespace PersonalFinance.Models
         public string TrsCode { get; set; }
     public string TrsTitle { get; set; }
     public DateTime TrsDateTime { get; set; }
-    public int TrsValue { get; set; }
+    public float TrsValue { get; set; }
     public string TrsNote { get; set; }
     }
 }
