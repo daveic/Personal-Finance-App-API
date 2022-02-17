f4bb9385d304eb252ffc19b9913979ab3cc12ece
Matteo Tumiati <setumiatim@tetrapak.com>
2022-02-17 12:18:03 +0100

    adds

--- Models/Expirations.cs
+++ Models/Expirations.cs
@@ -7,9 +7,9 @@ namespace PersonalFinance.Models
         public string Usr_OID { get; set; }
         public int ID { get; set; }
         public string ExpTitle { get; set; }
+        public string ExpDescription { get; set; }
         public double ExpValue { get; set; } //Diffenziato In - Out
         public string input_value { get; set; }
         public DateTime ExpDateTime { get; set; } //Data scadenza
-        public string ExpDescription { get; set; }
     }
 }
