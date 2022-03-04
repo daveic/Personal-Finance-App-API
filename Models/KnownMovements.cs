2e2fb13e118f5848a364017068ab2fae1cdb0b20
daveic <88085267+daveic@users.noreply.github.com>
2022-03-04 14:54:19 +0100

    add

--- Models/KnownMovements.cs
+++ Models/KnownMovements.cs
@@ -21,8 +21,6 @@ namespace PersonalFinance.Models
 
     public class KnownMovements_API
     {
-        public KnownMovement KnownMovement { get; set; }
         public IEnumerable<KnownMovement> KnownMovements { get; set; }
-        public IEnumerable<Expiration> Expirations { get; set; }
     }
 }
