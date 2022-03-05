5ccc1ee3b36df487059b43f002a65a0e49d8571c
daveic <88085267+daveic@users.noreply.github.com>
2022-03-05 16:23:26 +0100

    test 2

--- Controllers/KnownMovementsController.cs
+++ Controllers/KnownMovementsController.cs
@@ -97,5 +97,50 @@ namespace PersonalFinance.Controllers
             }
             return Ok(1);
         }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [NonAction]
+        public async Task ExpToRemoveAsync(string titleToMatch, string Usr_OID, int ID)
+        {
+            var expirations = await repo.GetAllExpirationsAsync();
+            int maxExp = expirations.Where(x => x.Usr_OID == Usr_OID).OrderBy(x => x.ID).Last().ID;
+            int i = 0;
+            bool is_equal = true;
+            while (is_equal)
+            {
+                Expiration e = await repo.GetExpirationAsync(ID + i);
+                if (e != null && e.ExpTitle == titleToMatch)
+                {
+                    var t = await repo.GetExpirationAsync(e.ID);
+                    await repo.DeleteExpirationAsync(t);
+                    await repo.SaveChangesAsync();
+                }
+                else if (e != null && e.ExpTitle != titleToMatch)
+                {
+                    is_equal = false;
+                }
+                else if (ID + i >= maxExp)
+                {
+                    is_equal = false;
+                }
+                i++;
+            }
+        }
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [NonAction]
+        public async Task<KnownMovement> EditKnownMovement(KnownMovement_Ext k)
+        {
+            if (k.KMValue < 0) k.KMType = "Uscita"; else if (k.KMValue >= 0) k.KMType = "Entrata";
+            if (k.On_Exp is true) k.Exp_ID = -1;
+            if (k.On_Exp is false)
+            {
+                string titleToMatch = k.KMTitle;
+                await ExpToRemoveAsync(titleToMatch, k.Usr_OID, k.Exp_ID);
+                k.Exp_ID = 0;
+            }
+            await repo.UpdateKnownMovementAsync(k);
+            await repo.SaveChangesAsync();
+            return k;
+        }
     }
 }
