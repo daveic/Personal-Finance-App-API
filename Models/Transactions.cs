85156950776ef28388a17b91143c5bd2d902a820
daveic <88085267+daveic@users.noreply.github.com>
2022-09-05 15:17:45 +0200

    1

--- Models/Transactions.cs
+++ Models/Transactions.cs
@@ -10,6 +10,7 @@ namespace PersonalFinance.Models
         public string Usr_OID { get; set; }
         public int ID { get; set; }
         public string TrsCode { get; set; }
+        public string TrsBank { get; set; }
         public string TrsTitle { get; set; }
         public DateTime TrsDateTime { get; set; }
         public double TrsValue { get; set; }
